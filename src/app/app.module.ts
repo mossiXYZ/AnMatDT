187ceb3024001d01a7239565ee205c72e60ed65b
mossi <mossi.hashemi@gmail.com>
2019-04-20 12:17:06 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomMaterialModule } from './shared/custommaterial/custommaterial.module';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
